2a878a5ff7ec974f96ec41aedeed320daa9e61b1
Vejber <vejber2013@gmail.com>
2022-08-08 14:12:22 +0400

    Added четное нечетное

---
+++ Seminar1/Homework/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> Четное
+
+int Prompt(string message)
+{
+System.Console.Write(message); // Вывести сообщение
+string readValue = Console.ReadLine(); // Считавает с консоли строку
+int result = int.Parse(readValue); // Преобрадует строку в целое число
+return result; // Возвращает результат
+}
+
+int n = Prompt("Enter a number > ");
+if(n%2==0)
+{
+  System.Console.WriteLine($"{n} - четное");
+}
+else
+{
+  System.Console.WriteLine($"{n} - нечетное");
+}
