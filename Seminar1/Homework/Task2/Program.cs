def1c3ee3856d181d8618cf444123ca437ee6aae
Vejber <vejber2013@gmail.com>
2022-08-08 14:05:43 +0400

    Added max of 3

---
+++ Seminar1/Homework/Task2/Program.cs
@@ -0,0 +1,75 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+int Prompt(string message)
+{
+System.Console.Write(message); // Вывести сообщение
+string readValue = Console.ReadLine(); // Считавает с консоли строку
+int result = int.Parse(readValue); // Преобрадует строку в целое число
+return result; // Возвращает результат
+}
+
+int a = Prompt("Enter the first number > ");
+int b = Prompt("Enter the second number > ");
+int c = Prompt("Enter the third number > ");
+int max = a;
+if (a == b && b == c)
+{
+  System.Console.WriteLine("There is no min or max");
+}
+if (a == b)
+{
+  if(a>c)
+  {
+    max = a;
+    System.Console.WriteLine($"max = {max}");
+  }
+  else if(c>a)
+  {
+    max = c;
+    System.Console.WriteLine($"max = {max}");
+  }
+}
+else if(b == c)
+{
+  if(b>a)
+  {
+    max = b;
+    System.Console.WriteLine($"max = {max}");
+  }
+  if(a>b)
+  {
+   max = a;
+  System.Console.WriteLine($"max = {max}");
+   }
+}
+else if(a == c)
+{
+  if(a>b)
+  {
+    max = a;
+    System.Console.WriteLine($"max = {max}");
+  }
+  if(b>a)
+  {
+    max = b;
+    System.Console.WriteLine($"max = {max}");
+  }
+}
+else if (a>max)
+{
+  max = a;
+  System.Console.WriteLine($"max = {max}");
+}
+else if(b>max)
+{
+  if(b>c)
+  {
+    max = b;
+    System.Console.WriteLine($"max = {max}");
+  }
+  else if(c>b)
+  {
+    max = c;
+    System.Console.WriteLine($"max = {max}");
+  }
+}
