dd1151d2dc07349dc7db7e1848680ea44a1129aa
Vejber <vejber2013@gmail.com>
2022-08-08 14:31:32 +0400

    Added division by 2

---
+++ Seminar1/Homework/Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+int Prompt(string message)
+{
+System.Console.Write(message); // Вывести сообщение
+string readValue = Console.ReadLine(); // Считавает с консоли строку
+int result = int.Parse(readValue); // Преобрадует строку в целое число
+return result; // Возвращает результат
+}
+
+int n = Prompt("Enter a number");
+int result = 1;
+if(n>0)
+{
+  while(result < n+1)
+  {
+    if(result%2==0)
+    {
+      System.Console.WriteLine($"{result}");
+    }
+    result++;
+  }
+}
+if(n<0)
+{
+  while(result > n)
+  {
+    if(n%2==0)
+    {
+      System.Console.WriteLine($"{n}");
+    }
+    n++;
+  }
+}
+else if(n==0)
+{
+  System.Console.WriteLine($"{n}");
+}
