a67dd784114bf244459eee6d03b40b273c7450e6
Vejber <vejber2013@gmail.com>
2022-08-10 19:17:35 +0400

    Added fill array call

---
+++ Seminar2/Homework/Task2/Program.cs
@@ -0,0 +1,16 @@
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+void FillArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = 0;
+  while(index<length)
+  {
+    collection[index] = new Random().Next(1,10);
+    index++;
+  }
+}
+int[]array = new int[3]; //создай новый массив с 3 элементами, по умолчанию все нули
+FillArray(array); //заменяет нули на цифры
+Console.Write($"{array[0]}{array[1]}{array[2]} -> ");
+Console.WriteLine($"{array[0]}{array[2]}");
