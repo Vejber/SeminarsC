369ad14565a2158faaf4103be102cc361a4fc683
Vejber <vejber2013@gmail.com>
2022-08-12 12:08:48 +0400

    fixed function

--- Seminar2/Homework/Task2/Program.cs
+++ Seminar2/Homework/Task2/Program.cs
@@ -9,9 +9,9 @@ void FillArray(int[] collection)
     collection[index] = new Random().Next(0,10);
     index++;
   }
+  collection[0] = new Random().Next(1,10); //избегаем случая когда у трехзначного числа первая цифра 0
 }
 int[]array = new int[3]; //создай новый массив с 3 элементами, по умолчанию все нули
 FillArray(array); //заменяет нули на цифры
-array[0] = new Random().Next(1,10);
 Console.Write($"{array[0]}{array[1]}{array[2]} -> ");
 Console.WriteLine($"{array[0]}{array[2]}");
