363985584952b9685580923c245c8755028c60f0
Vejber <105496392+Vejber@users.noreply.github.com>
2022-08-27 10:45:19 +0400

    copy array by elements

---
+++ Seminar6/Task5/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считавает с консоли строку
+    int result = int.Parse(readValue); // Преобрадует строку в целое число
+    return result; // Возвращает результат
+}
+
+int[] CopyArray(int[] n)
+{
+    int[] copyArray = new int[n.Length];
+    int index = 0;
+    int length = n.Length;
+    for (index = 0; index < length; index++)
+    {
+        copyArray[index] = n[index];
+    }
+    return copyArray;
+}
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+int[] num = new int[10];
+FillArray(num);
+Console.WriteLine($"{(string.Join(",", num))}");
+int[] copy = CopyArray(num);
+Console.WriteLine($"{(string.Join(",", copy))}");
+// num[1] = 11;
+// Console.WriteLine($"{(string.Join(",", num))}");
+// copy = CopyArray(num);
+// Console.WriteLine($"{(string.Join(",", copy))}");
