60671310e0351fdf8325acae61dd4cbad2614244
Vejber <105496392+Vejber@users.noreply.github.com>
2022-08-30 12:07:01 +0400

    Find total of the main diagonal in array

---
+++ Seminar7/Task4/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+// Например, задан массив:
+// 1   4   7
+// 5   9   2
+// 8   4   2
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобразует строку в целое число
+    return result; // Возвращает результат
+}
+
+int FindDiagonal(int[,] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                count = count + array[i, j];
+            }
+        }
+    }
+    return count;
+}
+
+
+
+int m = Prompt("Enter the number of rows in a table > ");
+int n = Prompt("Enter the number of columns in a table > ");
+if (m > 0 && n > 0)
+{
+    int[,] matrix = new int[m, n];
+    FillArray(matrix);
+    PrintArray(matrix);
+    Console.WriteLine();
+    int Count = FindDiagonal(matrix);
+    Console.WriteLine($"-> {Count}");
+}
+else
+{
+    Console.WriteLine("Number of row and columns must be positive.");
+}
+
+
+
