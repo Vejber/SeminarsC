d3c4cde49268ae36afdd13adfc561b6e2d040dfc
Vejber <105496392+Vejber@users.noreply.github.com>
2022-08-31 18:12:31 +0400

    Exchange first and last rows in a multi array

---
+++ Seminar8/Task1/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобразует строку в целое число
+    return result; // Возвращает результат
+}
+
+int[,] ExchangeFirstForLast(int[,] arr) 
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if ( i == 0)
+            {
+                int temp = arr[i, j]; //first
+                arr[i, j] = arr[arr.GetLength(0)-1, j]; //last
+                arr[arr.GetLength(0)-1, j] = temp;
+            }
+            
+        }
+
+    }
+    return arr;
+}
+
+
+
+int m = Prompt("Enter the number of rows in a table > ");
+int n = Prompt("Enter the number of columns in a table > ");
+if (m > 0 && n > 0)
+{
+    int[,] matrix = new int[m, n];
+    FillArray(matrix);
+    PrintArray(matrix);
+    Console.WriteLine();
+    ExchangeFirstForLast(matrix);
+    PrintArray(matrix);
+}
+else
+{
+    Console.WriteLine("The number of rows and columns must be above zero");
+}
