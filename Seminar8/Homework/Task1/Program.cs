f930f5cce9f2d9383e2e7bbcce13eadfda67ea1a
Vejber <105496392+Vejber@users.noreply.github.com>
2022-09-01 17:01:25 +0400

    Sort rows descending in multi array

---
+++ Seminar8/Homework/Task1/Program.cs
@@ -0,0 +1,78 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобразует строку в целое число
+    return result; // Возвращает результат
+}
+
+int[,] SortRowDescending(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int i_compare = 0; i_compare < arr.GetLength(1) - 1; i_compare++)
+            {
+                if (arr[i, i_compare] < arr[i, i_compare + 1])
+                {
+                    int temp = arr[i, i_compare];
+                    arr[i, i_compare] = arr[i, i_compare + 1];
+                    arr[i, i_compare + 1] = temp;
+                }
+            }
+        }
+    }
+    return arr;
+}
+
+
+
+int m = Prompt("Enter the number of rows in a table > ");
+int n = Prompt("Enter the number of columns in a table > ");
+if (m > 0 && n > 0)
+{
+    int[,] matrix = new int[m, n];
+    FillArray(matrix);
+    PrintArray(matrix);
+    Console.WriteLine();
+    matrix = SortRowDescending(matrix);
+    PrintArray(matrix);
+}
+else
+{
+    Console.WriteLine("The number of rows and columns must be above zero");
+}
