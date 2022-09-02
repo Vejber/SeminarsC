c229166bd9e33577abd24e7f67ff9cd35b5e911e
Vejber <105496392+Vejber@users.noreply.github.com>
2022-09-02 18:17:08 +0400

    Recursion sum of digits

---
+++ Seminar9/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. 
+// Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобразует строку в целое число
+    return result; // Возвращает результат
+}
+
+int SumOfDigits(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    return SumOfDigits(n / 10) + n % 10;
+}
+
+int N = Prompt("Enter a number > ");
+int sum = SumOfDigits(N);
+Console.WriteLine(sum);
+
+
