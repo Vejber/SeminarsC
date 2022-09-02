095e2eef61a3cc52170cc7752b908739e3a0a5c7
Vejber <105496392+Vejber@users.noreply.github.com>
2022-09-02 17:23:43 +0400

    Multiply matrix

--- Seminar8/Homework/Task3/Program.cs
+++ Seminar8/Homework/Task3/Program.cs
@@ -46,7 +46,7 @@ int[,] MatrixMultiply(int[,] arr1, int[,] arr2)
         for (int j = 0; j < arr2.GetLength(1); j++)
         {
             result[i, j] = 0;
-            for (int k = 0; k < arr2.GetLength(1); k++)
+            for (int k = 0; k < arr1.GetLength(1); k++)
             {
                 result[i, j] = (arr1[i, k] * arr2[k, j]);
             }
