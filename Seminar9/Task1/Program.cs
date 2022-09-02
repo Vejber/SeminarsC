114a894ab9e3fa5853ebf13de82da563ac6a34c0
Vejber <105496392+Vejber@users.noreply.github.com>
2022-09-02 16:45:40 +0400

    Recursion easy

---
+++ Seminar9/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// адайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Вывести сообщение
+    string readValue = Console.ReadLine(); // Считывает с консоли строку
+    int result = int.Parse(readValue); // Преобразует строку в целое число
+    return result; // Возвращает результат
+}
+
+void ShowTillN(int n)
+{
+    if(n <= 0)
+    {
+        return;
+    }
+    ShowTillN(n-1);
+    Console.WriteLine(n);
+}
+
+int N = Prompt("Enter a number > ");
+ShowTillN(N);
